a229aae9203733c0f0edd703edac928c75d02eed
Thomas Nicholson <nicholmen@gmail.com>
2017-11-09 04:52:55 +0000

    completed admin categories functionality:

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,4 +5,12 @@ class CategoriesController < ApplicationController
     @products = @category.products.order(created_at: :desc)
   end
 
+  def index
+    @categories = Category.all.order(created_at: :desc)
+  end
+
+  # def show
+  #   @category = Product.find params[:id]
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,20 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
-  resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
     put    :add_item
     delete :remove_item
   end
 
+  resources :categories, only: [:index, :show]
+  
   resources :orders, only: [:create, :show]
 
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
